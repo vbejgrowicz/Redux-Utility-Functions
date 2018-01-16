a87edebc14fd3f16b45a7f9be23401d91386173a
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-15 22:18:11 -0500

    Feature: Add functions for each and filter

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 (function(window){
   var library = {};
 
-  library.alert = function() {
-    alert('Alert from Library');
+  // Object/Array Functions
+  library.each = function(input, func) {
+    if (input instanceof Object && func instanceof Function) {
+      for (key in input) {
+        func(key, input[key]);
+      }
+      return input;
+    }
+    return undefined;
+  }
+
+  library.filter = function(input, func) {
+    if (input instanceof Object && func instanceof Function) {
+      var newArray = [];
+      this.each(input, function(key, value){
+        if (func(input[key])) {
+          newArray.push(input[key])
+        }
+      });
+      return newArray;
+    }
+    return undefined;
   }
 
   window.$l = library;
